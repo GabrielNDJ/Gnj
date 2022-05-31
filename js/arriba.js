b9943af3e2feb22bccb952e16d06ad2dbd503d81
your_github_username <gabrielnietodejesus@gmail.com>
2022-05-31 06:10:07 -0300

    Reparacion del boton up

--- js/arriba.js
+++ js/arriba.js
@@ -1,18 +1,9 @@
-$(document).ready(function(){
-   $('.ir-arriba').click(function() {
+const toTop = document.querySelector(".to-top");
 
-   $('body, html').animate ({
-       scrollTop: '0px'
-   }, 300)
-});
-
-$(window).scroll(function(){
-    if($(this).scrollTop() > 0){
-
-        $('.ir-arriba').slideDown (300);
-        
-    } else {
-        $('.ir-arriba').slideUp (300);
-    }
-});
-});
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 1000) {
+    toTop.classList.add("active");
+  } else {
+    toTop.classList.remove("active");
+  }
+})
